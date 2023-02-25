95cb915dc172b5f9286e1b88397eef5eccd6b469
cringemoment <69696899+cringemoment@users.noreply.github.com>
2023-02-25 15:34:37 -0500

    Add files via upload

---
+++ gettopruns.py
@@ -0,0 +1,853 @@
+thisweek = '''
+Jstris
+BOT
+ — Today at 2:49 AM
+Holifyre - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by Holifyre!
+Time
+13:15.605 min
+Mode
+PC Mode
+Player
+Holifyre
+PPS
+0.993
+PCs
+80
+Blocks
+790
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — Today at 2:14 AM
+Holifyre - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by Holifyre!
+Time
+18:20.912 min
+Mode
+PC Mode
+Player
+Holifyre
+PPS
+1.063
+PCs
+119
+Blocks
+1170
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — Today at 2:07 AM
+13pake - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by 13pake!
+Time
+14:09.754 min
+Mode
+PC Mode
+Player
+13pake
+PPS
+0.841
+PCs
+72
+Blocks
+715
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — Yesterday at 10:21 PM
+JstrisAllClear - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by JstrisAllClear!
+Time
+11:56.936 min
+Mode
+PC Mode
+Player
+JstrisAllClear
+PPS
+0.837
+PCs
+60
+Blocks
+600
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — Yesterday at 3:58 PM
+Holifyre - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by Holifyre!
+Time
+18:58.876 min
+Mode
+PC Mode
+Player
+Holifyre
+PPS
+0.997
+PCs
+115
+Blocks
+1135
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — Yesterday at 1:01 PM
+13pake - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by 13pake!
+Time
+6:33.222 min
+Mode
+PC Mode
+Player
+13pake
+PPS
+1.017
+PCs
+41
+Blocks
+400
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — Yesterday at 6:47 AM
+Holifyre - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by Holifyre!
+Time
+12:36.650 min
+Mode
+PC Mode
+Player
+Holifyre
+PPS
+0.753
+PCs
+58
+Blocks
+570
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/18/2023 10:41 PM
+Fragmented - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by Fragmented!
+Time
+8:19.553 min
+Mode
+PC Mode
+Player
+Fragmented
+PPS
+0.941
+PCs
+47
+Blocks
+470
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/18/2023 1:42 PM
+Fragmented - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by Fragmented!
+Time
+9:47.817 min
+Mode
+PC Mode
+Player
+Fragmented
+PPS
+1.225
+PCs
+72
+Blocks
+720
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/18/2023 9:32 AM
+Holifyre - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by Holifyre!
+Time
+12:52.635 min
+Mode
+PC Mode
+Player
+Holifyre
+PPS
+1.022
+PCs
+80
+Blocks
+790
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/18/2023 5:23 AM
+Holifyre - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by Holifyre!
+Time
+7:35.087 min
+Mode
+PC Mode
+Player
+Holifyre
+PPS
+1.307
+PCs
+60
+Blocks
+595
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/18/2023 3:09 AM
+13pake - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by 13pake!
+Time
+14:11.119 min
+Mode
+PC Mode
+Player
+13pake
+PPS
+1.099
+PCs
+94
+Blocks
+935
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/18/2023 2:27 AM
+13pake - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by 13pake!
+Time
+8:17.076 min
+Mode
+PC Mode
+Player
+13pake
+PPS
+1.036
+PCs
+52
+Blocks
+515
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/18/2023 1:21 AM
+Holifyre - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by Holifyre!
+Time
+7:14.162 min
+Mode
+PC Mode
+Player
+Holifyre
+PPS
+1.14
+PCs
+51
+Blocks
+495
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/18/2023 12:26 AM
+Tiki2TGT - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by Tiki2TGT!
+Time
+10:51.429 min
+Mode
+PC Mode
+Player
+Tiki2TGT
+PPS
+0.814
+PCs
+54
+Blocks
+530
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/17/2023 6:12 PM
+13pake - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by 13pake!
+Time
+19:53.055 min
+Mode
+PC Mode
+Player
+13pake
+PPS
+0.855
+PCs
+103
+Blocks
+1020
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/17/2023 4:38 PM
+suwapete - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by suwapete!
+Time
+14:54.307 min
+Mode
+PC Mode
+Player
+suwapete
+PPS
+0.514
+PCs
+46
+Blocks
+460
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/17/2023 1:22 PM
+Fragmented - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by Fragmented!
+Time
+11:54.294 min
+Mode
+PC Mode
+Player
+Fragmented
+PPS
+1.022
+PCs
+74
+Blocks
+730
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/17/2023 12:55 PM
+13pake - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by 13pake!
+Time
+22:34.071 min
+Mode
+PC Mode
+Player
+13pake
+PPS
+0.761
+PCs
+104
+Blocks
+1030
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/17/2023 5:45 AM
+Fragmented - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by Fragmented!
+Time
+7:16.732 min
+Mode
+PC Mode
+Player
+Fragmented
+PPS
+1.122
+PCs
+49
+Blocks
+490
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/16/2023 4:10 PM
+Fragmented - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by Fragmented!
+Time
+6:29.599 min
+Mode
+PC Mode
+Player
+Fragmented
+PPS
+1.04
+PCs
+41
+Blocks
+405
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/16/2023 3:39 PM
+Fragmented - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by Fragmented!
+Time
+9:25.673 min
+Mode
+PC Mode
+Player
+Fragmented
+PPS
+0.99
+PCs
+57
+Blocks
+560
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/16/2023 2:00 PM
+13pake - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by 13pake!
+Time
+4:54.023 min
+Mode
+PC Mode
+Player
+13pake
+PPS
+1.599
+PCs
+47
+Blocks
+470
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/16/2023 11:49 AM
+13pake - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by 13pake!
+Time
+9:06.298 min
+Mode
+PC Mode
+Player
+13pake
+PPS
+0.879
+PCs
+48
+Blocks
+480
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/16/2023 7:14 AM
+Holifyre - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by Holifyre!
+Time
+4:46.277 min
+Mode
+PC Mode
+Player
+Holifyre
+PPS
+1.502
+PCs
+44
+Blocks
+430
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/16/2023 5:00 AM
+Holifyre - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by Holifyre!
+Time
+10:47.400 min
+Mode
+PC Mode
+Player
+Holifyre
+PPS
+1.112
+PCs
+73
+Blocks
+720
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/15/2023 9:13 PM
+JstrisAllClear - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by JstrisAllClear!
+Time
+29:50.116 min
+Mode
+PC Mode
+Player
+JstrisAllClear
+PPS
+0.782
+PCs
+140
+Blocks
+1400
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/15/2023 2:11 PM
+13pake - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by 13pake!
+Time
+17:44.460 min
+Mode
+PC Mode
+Player
+13pake
+PPS
+0.77
+PCs
+82
+Blocks
+820
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/15/2023 1:05 PM
+Fragmented - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by Fragmented!
+Time
+9:20.083 min
+Mode
+PC Mode
+Player
+Fragmented
+PPS
+1.027
+PCs
+58
+Blocks
+575
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/15/2023 12:14 PM
+Holifyre - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by Holifyre!
+Time
+6:28.326 min
+Mode
+PC Mode
+Player
+Holifyre
+PPS
+1.069
+PCs
+42
+Blocks
+415
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/14/2023 10:18 PM
+Holifyre - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by Holifyre!
+Time
+5:50.072 min
+Mode
+PC Mode
+Player
+Holifyre
+PPS
+1.314
+PCs
+46
+Blocks
+460
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/14/2023 5:50 PM
+cringemoment - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by cringemoment!
+Time
+5:46.144 min
+Mode
+PC Mode
+Player
+cringemoment
+PPS
+1.531
+PCs
+53
+Blocks
+530
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/14/2023 2:49 PM
+Fragmented - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by Fragmented!
+Time
+10:39.001 min
+Mode
+PC Mode
+Player
+Fragmented
+PPS
+0.876
+PCs
+57
+Blocks
+560
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/14/2023 2:40 PM
+13pake - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by 13pake!
+Time
+9:50.225 min
+Mode
+PC Mode
+Player
+13pake
+PPS
+0.771
+PCs
+46
+Blocks
+455
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/14/2023 2:12 PM
+Fragmented - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by Fragmented!
+Time
+6:54.065 min
+Mode
+PC Mode
+Player
+Fragmented
+PPS
+0.978
+PCs
+41
+Blocks
+405
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/14/2023 1:13 PM
+13pake - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by 13pake!
+Time
+10:00.825 min
+Mode
+PC Mode
+Player
+13pake
+PPS
+0.741
+PCs
+45
+Blocks
+445
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/14/2023 9:04 AM
+JohnBeak - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by JohnBeak!
+Time
+9:06.470 min
+Mode
+PC Mode
+Player
+JohnBeak
+PPS
+0.741
+PCs
+41
+Blocks
+405
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/14/2023 3:08 AM
+13pake - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by 13pake!
+Time
+15:22.084 min
+Mode
+PC Mode
+Player
+13pake
+PPS
+0.965
+PCs
+89
+Blocks
+890
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/13/2023 6:03 PM
+Fragmented - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by Fragmented!
+Time
+5:31.035 min
+Mode
+PC Mode
+Player
+Fragmented
+PPS
+1.208
+PCs
+41
+Blocks
+400
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/13/2023 5:01 PM
+13pake - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by 13pake!
+Time
+15:37.211 min
+Mode
+PC Mode
+Player
+13pake
+PPS
+0.864
+PCs
+82
+Blocks
+810
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/13/2023 4:30 PM
+torchlight - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by torchlight!
+Time
+14:10.911 min
+Mode
+PC Mode
+Player
+torchlight
+PPS
+0.546
+PCs
+48
+Blocks
+465
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/13/2023 1:47 AM
+13pake - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by 13pake!
+Time
+14:05.413 min
+Mode
+PC Mode
+Player
+13pake
+PPS
+0.875
+PCs
+74
+Blocks
+740
+
+Jstris Bot Stats v0.6
+Jstris
+BOT
+ — 02/13/2023 12:49 AM
+13pake - Over 41 PCs in PC Mode
+Achievement Over 41 PCs in PC Mode (game mode PC Mode) played by 13pake!
+Time
+7:55.063 min
+Mode
+PC Mode
+Player
+13pake
+PPS
+1.031
+PCs
+50
+Blocks
+490
+'''
+
+runs = thisweek.split("Jstris Bot Stats v0.6")
+runvalues = []
+
+for i in runs:
+    try:
+        pcplayer = i[i.index("played by ") + 10 : i.index("!\nTime")]
+        pcnum = (i[i.index("PCs\n") + 3 : i.index("Blocks")]).split("\n")[1]
+        runvalues.append([pcplayer, int(pcnum)])
+    except ValueError:
+        pass
+
+runvalues.sort(key=lambda x: x[1], reverse = True)
+runvalueswithoutrepeat = []
+for i in runvalues:
+    for j in runvalueswithoutrepeat:
+        if(i[0] == j[0]):
+            break
+    else:
+        runvalueswithoutrepeat.append(i)
+
+print("Top runs")
+[print(i) for i in runvalues]
+longestlength = 0
+longestpblength = 0
+
+for i in range(5):
+    if(len(runvalueswithoutrepeat[i][0]) > longestlength):
+        longestlength = len(runvalueswithoutrepeat[i][0])
+    if(len(str(runvalueswithoutrepeat[i][1])) > longestpblength):
+        longestpblength = len(str(runvalueswithoutrepeat[i][1]))
+
+
+
+for i in range(5):  
+    print(str(i + 1) + ". " + str(runvalueswithoutrepeat[i][0]) + (" " * (longestlength - len(runvalueswithoutrepeat[i][0]) + 2 + longestpblength - len(str(runvalueswithoutrepeat[i][1])))) + str(runvalueswithoutrepeat[i][1]) + " PC")
